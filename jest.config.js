8cfdf57ee69e263f049bbd535d6de8f1c41aaed4
narvidas <m.narvidas@gmail.com>
2019-09-17 20:33:27 +0100

    Prettify whole app to ensure it conforms to ruleset

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  setupFilesAfterEnv: ["<rootDir>/rtl.setup.js"],
-  testPathIgnorePatterns: ["node_modules"],
-  coveragePathIgnorePatterns: ["node_modules"],
-  reporters: ["default", ["jest-junit", { outputDirectory: "testReport" }]],
-  coverageDirectory: "testReport",
-  coverageReporters: ["text", "cobertura", "lcov"]
+  setupFilesAfterEnv: ['<rootDir>/rtl.setup.js'],
+  testPathIgnorePatterns: ['node_modules'],
+  coveragePathIgnorePatterns: ['node_modules'],
+  reporters: ['default', ['jest-junit', { outputDirectory: 'testReport' }]],
+  coverageDirectory: 'testReport',
+  coverageReporters: ['text', 'cobertura', 'lcov'],
 };
