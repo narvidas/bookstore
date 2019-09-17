d40d993ad98fc9254256989744010aa6060e0081
narvidas <m.narvidas@gmail.com>
2019-09-17 17:58:18 +0100

    Prettier conf, absolute imports, additional packages

--- rtl.setup.js
+++ rtl.setup.js
@@ -1,4 +1,5 @@
-import "core-js";
-import "core-js/es6/map";
-import "@testing-library/jest-dom/extend-expect";
-import "@testing-library/react/cleanup-after-each";
+import 'core-js';
+import 'core-js/es6/map';
+import 'core-js/es6/set';
+import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/react/cleanup-after-each';
