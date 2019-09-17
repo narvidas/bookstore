8b5b959bde062d52481036802b27fa9d9764cb4a
narvidas <m.narvidas@gmail.com>
2019-09-17 18:00:02 +0100

    View component naming convention, correct gql enpoint url

--- src/App.js
+++ src/App.js
@@ -1,39 +1,24 @@
-import React from "react";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import TypoGraphy from "@material-ui/core/Typography";
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect
-} from "react-router-dom";
-import ApolloClient, { InMemoryCache } from "apollo-boost";
-import { Create } from "./views/create";
-import { Edit } from "./views/edit";
-import { List } from "./views/list";
-import { ApolloProvider } from "@apollo/react-hooks";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import ApolloClient, { InMemoryCache } from 'apollo-boost';
+import { ListView } from './views/ListView';
+import { CreateView } from './views/CreateView';
+import { EditView } from './views/EditView';
+import { ApolloProvider } from '@apollo/react-hooks';
 
 const cache = new InMemoryCache();
 const client = new ApolloClient({
-  uri: "http://localhost:4567",
-  cache
+  uri: 'http://localhost:4567/graphql',
+  cache,
 });
 
 const App = () => (
   <ApolloProvider client={client}>
-    <AppBar color="primary" position="static">
-      <Toolbar>
-        <TypoGraphy variant="title" color="inherit">
-          Bookstore
-        </TypoGraphy>
-      </Toolbar>
-    </AppBar>
     <Router>
       <Switch>
-        <Route path="/list" exact component={List} />
-        <Route path="/edit/:id" exact component={Edit} />
-        <Route path="/create/:id" component={Create} />
+        <Route path="/list" exact component={ListView} />
+        <Route path="/create" component={CreateView} />
+        <Route path="/edit/:bookId" exact component={EditView} />
         <Redirect exact from="/" to="list" />
       </Switch>
     </Router>
